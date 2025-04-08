fa081c1ee5b2d7abc153e83c448eeebfc106bb02
Abderahim <ita27rmp100@gmail.com>
2025-04-08 16:29:25 +0100

    second part of third exo

--- serie03/exo3p2.c
+++ serie03/exo3p2.c
@@ -7,41 +7,35 @@
 typedef struct node
     {
         int data;
-        struct node *next
+        struct node *next;
     }node;
 typedef struct{
-        int arr[max];
-        int top;
+        node *top;
     }Stack;
     
 
 void CreateStack(Stack *S){
-    S->top = -1;
-}
-bool isFull(Stack *S){
-    return S->top ==max;
+    S->top = NULL;
 }
 bool isEmpty(Stack *S){
-    return S->top==-1;
+    return S->top==NULL;
 }
 void push(Stack *S,int *val){
-    if (isFull(S))
-    {
-        printf("This Stack is full \n");
-    }
-    else{
-        S->arr[S->top] = *val;
-        S->top = S->top +1;
-        printf("VALUE ADDED \n");
-    }
+    node *p = (node*)malloc(sizeof(node));
+    p->next = S->top; 
+    p->data = *val;
+    S->top = p;
+    printf("VALUE ADDED \n");
 }
 void pop(Stack *S,int *c){
     if(isEmpty(S)){
         printf("Nothing to pop it");
     }
     else{
-        *c = S->arr[S->top];
-        S->top = S->top - 1;
+        node *p = S->top;
+        *c = S->top->data;
+        S->top = S->top->next;
+        free(p);
     }
 }
 
@@ -51,16 +45,9 @@ int main(){
     CreateStack(&S);
     for (int i = 0; i < 5; i++)
     {
-        if (!isFull(&S))
-        {
-            printf("Enter a value : ");
-            scanf("%i",&value);
-            push(&S,&value);
-        }
-        else{
-            printf("This Stack is empty \n");
-            break;
-        }
+        printf("Enter a value : ");
+        scanf("%i",&value);
+        push(&S,&value);
     }
     for (int i = 0; i < 5; i++)
     {
